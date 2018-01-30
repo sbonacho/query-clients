9808ac3ada0575b9d4f4bef49a0354d5be5e252a
Santiago Garcia-Bonacho Rico <sbonacho@gmail.com>
2018-01-30 17:14:11 +0100

    feat(): make functional testing

--- src/test/java/com/soprasteria/seda/examples/insurance/api/ClientsQueryBootTests.java
+++ src/test/java/com/soprasteria/seda/examples/insurance/api/ClientsQueryBootTests.java
@@ -2,31 +2,69 @@ package com.soprasteria.seda.examples.insurance.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.soprasteria.seda.examples.insurance.events.ClientPortfolioCompleted;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.MessageListenerContainer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.rule.KafkaEmbedded;
+import org.springframework.kafka.test.utils.ContainerTestUtils;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Map;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 @DirtiesContext
-@EmbeddedKafka(partitions = 1)
+@EmbeddedKafka(partitions = 1, topics = { ClientsQueryBootTests.saga })
 public class ClientsQueryBootTests {
 
-    private static final String saga = "saga";
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Autowired
+    protected static final String saga = "saga";
+
 	private KafkaTemplate<String, Object> kafkaTemplate;
 
 	private ObjectMapper mapper = new ObjectMapper();
 
+    @Autowired
+    private KafkaEmbedded embeddedKafka;
 
-	@Test
+
+    @Before
+    public void setUp() throws Exception {
+        // set up the Kafka producer properties
+        Map<String, Object> senderProperties = KafkaTestUtils.senderProps(embeddedKafka.getBrokersAsString());
+        senderProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        senderProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        ProducerFactory<String, Object> producerFactory = new DefaultKafkaProducerFactory<String, Object>(senderProperties);
+
+        kafkaTemplate = new KafkaTemplate<>(producerFactory);
+    }
+
+    @Test
 	public void clientCreateCompletedStoreQuery() throws Exception {
 
         ClientPortfolioCompleted completed = mapper.readValue("{\"type\": \"ClientCreated\", \"name\": \"John Doe\", \"address\": \"Bendford st 10\", \"interest\": \"Microservices\", \"portfolioId\":\"ccaaf032-32fe-4476-9ef2-9a70d18a58b0\",\"clientId\":\"82565d32-45ea-40c5-9f56-9d2a93a648a0\"}", ClientPortfolioCompleted.class);
@@ -34,8 +72,12 @@ public class ClientsQueryBootTests {
 		// Send mocked events --------------------
 
 		kafkaTemplate.send(saga, completed);
+        Thread.sleep(2000);
 
+        // Check if client is in DataBase
+        mockMvc.perform(get("/client")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString(completed.getAddress())));
 
-	}
+    }
 
 }
