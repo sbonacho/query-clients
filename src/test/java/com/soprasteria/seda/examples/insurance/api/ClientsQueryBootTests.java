05bfce3fd5a935cc26e23bbf435777050fa41902
Santiago Garcia-Bonacho Rico <sbonacho@gmail.com>
2018-02-15 12:48:05 +0100

    fix(): change test in order to get it working on C.I.

--- src/test/java/com/soprasteria/seda/examples/insurance/api/ClientsQueryBootTests.java
+++ src/test/java/com/soprasteria/seda/examples/insurance/api/ClientsQueryBootTests.java
@@ -2,9 +2,13 @@ package com.soprasteria.seda.examples.insurance.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.soprasteria.seda.examples.insurance.events.ClientPortfolioCompleted;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.hamcrest.Matchers;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,19 +18,29 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.kafka.listener.MessageListener;
+import org.springframework.kafka.listener.config.ContainerProperties;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.kafka.test.rule.KafkaEmbedded;
+import org.springframework.kafka.test.utils.ContainerTestUtils;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -53,9 +67,13 @@ public class ClientsQueryBootTests {
     @Autowired
     private KafkaEmbedded embeddedKafka;
 
+    private KafkaMessageListenerContainer<String, Object> container;
+
+    private BlockingQueue<ConsumerRecord<String, Object>> records;
 
     @Before
     public void setUp() throws Exception {
+
         // set up the Kafka producer properties
         Map<String, Object> senderProperties = KafkaTestUtils.senderProps(embeddedKafka.getBrokersAsString());
         senderProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
@@ -64,6 +82,48 @@ public class ClientsQueryBootTests {
         ProducerFactory<String, Object> producerFactory = new DefaultKafkaProducerFactory<String, Object>(senderProperties);
 
         kafkaTemplate = new KafkaTemplate<>(producerFactory);
+
+        // set up the Kafka consumer properties
+        Map<String, Object> consumerProperties = KafkaTestUtils.consumerProps("testSaga", "false", embeddedKafka);
+        consumerProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+
+        // create a Kafka consumer factory
+        JsonDeserializer des = new JsonDeserializer<>(Object.class);
+        des.addTrustedPackages("com.soprasteria.seda.examples.insurance.events");
+        DefaultKafkaConsumerFactory<String, Object> consumerFactory = new DefaultKafkaConsumerFactory<String, Object>(consumerProperties, new StringDeserializer(), des);
+
+        // set the topic that needs to be consumed
+        ContainerProperties containerProperties = new ContainerProperties(saga);
+
+        // create a Kafka MessageListenerContainer
+        container = new KafkaMessageListenerContainer<>(consumerFactory, containerProperties);
+
+        // create a thread safe queue to store the received message
+        records = new LinkedBlockingQueue<>();
+
+        // setup a Kafka message listener
+        container.setupMessageListener(new MessageListener<String, Object>() {
+            @Override
+            public void onMessage(ConsumerRecord<String, Object> record) {
+                LOGGER.info("Tests Listener -> topic: {} - {}", record.topic(), record.value());
+                records.add(record);
+            }
+        });
+
+        // start the container and underlying message listener
+        container.start();
+
+        // wait until the container has the required number of assigned partitions
+        //for (MessageListenerContainer messageListenerContainer : kafkaListenerEndpointRegistry.getListenerContainers()) {
+        ContainerTestUtils.waitForAssignment(container, embeddedKafka.getPartitionsPerTopic());
+        //}
+    }
+
+    @After
+    public void tearDown() {
+        // stop the container
+        container.stop();
     }
 
     @Test
@@ -75,11 +135,16 @@ public class ClientsQueryBootTests {
 
         Thread.sleep(100);
 		kafkaTemplate.send(saga, completed);
-        Thread.sleep(100);
 
-        // Check if client is in DataBase
-        mockMvc.perform(get("/client")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(Matchers.containsString(completed.getAddress())));
+        ConsumerRecord<String, Object> rec = records.poll(2, TimeUnit.SECONDS);
+
+        assertThat(rec).isNotNull();
+
+        if (rec != null) {
+            // Check if client is in DataBase
+            mockMvc.perform(get("/client")).andDo(print()).andExpect(status().isOk())
+                    .andExpect(content().string(Matchers.containsString(completed.getAddress())));
+        }
     }
 
 }
